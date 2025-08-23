0d368333399377114f5e8e43037960ab3f261ccd
Edem <edem.quashigah@student.hamk.fi>
2025-08-23 11:19:23 +0300

    2.9 Created search bar feature

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123-333-4455" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   // Add a new name
   const addNewName = (event) => {
@@ -36,12 +40,27 @@ const App = () => {
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
+
+    // search each value for similar
   };
 
   // Set a new number
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
+  // handle filter
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  // List of persons to show after search
+  const filteredPersons =
+    filter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        );
 
   // Numbers component for displaying the list
   const Numbers = ({ persons }) => {
@@ -59,6 +78,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with{" "}
+        <input
+          placeholder="Search phonebook"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -73,14 +101,11 @@ const App = () => {
       <h2>Numbers</h2>
       {/* <Numbers persons={persons} /> */}
       <div>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <p key={person.name}>
             {person.name} {person.number}
           </p>
         ))}
-      </div>
-      <div>
-        debug: {newName} {newNumber}
       </div>
     </div>
   );
