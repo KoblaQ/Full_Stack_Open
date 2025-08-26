d052fb652ef87a1f2beb4b26afea69d7b153e277
Edem <edem.quashigah@student.hamk.fi>
2025-08-26 15:45:34 +0300

    2.11 phonebook
    Initial state of data fetched from server using axios.

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 // Single person component
 const Person = ({ persons }) => {
@@ -56,15 +57,19 @@ const Filter = ({ filter, handleFilterChange }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "notes");
 
   // Add a new name
   const addNewName = (event) => {
@@ -95,8 +100,6 @@ const App = () => {
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
-
-    // search each value for similar
   };
 
   // Set a new number
