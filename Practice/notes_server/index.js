32786ad6ebbf219a82f46a5a2428b1331d1ab236
Edem <edem.quashigah@student.hamk.fi>
2025-09-08 20:48:36 +0300

    Implemented dist for deploying to render

--- Practice/notes_server/index.js
+++ Practice/notes_server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -28,8 +29,11 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+// USES
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static("dist")); // Middleware for showing Static file from dist
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -90,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
