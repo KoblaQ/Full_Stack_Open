a0817ee8025b95f7f71ff927867d13d9be9e2667
Edem <edem.quashigah@student.hamk.fi>
2025-10-01 17:46:45 +0300

    4.2 complete

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-// const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 const app = express()
@@ -18,6 +18,11 @@ mongoose
   })
 
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- Part4/bloglist/index.js
+++ Part4/bloglist/index.js
@@ -1,19 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-// const Blog = require('./models/blog')
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// const app = express()
-
-// const mongoUrl = process.env.MONGODB_URI
-// mongoose.connect(mongoUrl)
-
-// app.use(express.json())
-
-const PORT = 3003
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
