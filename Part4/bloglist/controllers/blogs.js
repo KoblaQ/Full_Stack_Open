36d1046d1d8707723a5bb3f10b49865e5c4461b4
Edem <edem.quashigah@student.hamk.fi>
2025-10-08 15:30:27 +0300

    4.13

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -6,12 +6,26 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  // Adds like property at value 0 if omitted
+  if (!blog.likes) {
+    blog.likes = 0
+  }
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  // If the blog is missing a title or url
+  if (!blog.title || !blog.url) {
+    return response.status(400).end()
+  }
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+// Delete by ID
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- Part4/bloglist/tests/blog_api.test.js
+++ Part4/bloglist/tests/blog_api.test.js
@@ -1,10 +1,14 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 const helper = require('./test_helper')
+const Test = require('supertest/lib/test')
+const {
+  notesInDb,
+} = require('../../../Practice/notes/notes_server/tests/test_helper')
 
 const api = supertest(app)
 
@@ -49,6 +53,62 @@ test('a valid blog can be added', async () => {
   const blogsAtEnd = await helper.blogsInDb()
   assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
   assert.strictEqual(blogsAtEnd[blogsAtEnd.length - 1].title, newBlog.title)
+})
+
+test('verify that likes property defaults to value 0 if left out', async () => {
+  const newBlog = {
+    title: 'Testing post Blog',
+    author: 'Kafui',
+    url: 'https://fullstackopen.com/en/',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd[blogsAtEnd.length - 1].likes, 0)
+})
+
+describe('missing title or url', () => {
+  test('a blog without title cannot be added', async () => {
+    const newBlog = {
+      author: 'Kafui',
+      url: 'https://fullstackopen.com/en/',
+      likes: 99,
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+
+  test('a blog without url cannot be added', async () => {
+    const newBlog = {
+      title: 'Testing post Blog',
+      author: 'Kafui',
+      likes: 35,
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
+})
+
+test('delete a blog by ID', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToBeDeleted = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToBeDeleted.id}`).expect(204)
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const blogTitles = blogsAtEnd.map((blog) => blog.title)
+  assert(!blogTitles.includes(blogToBeDeleted.title))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 3)
 })
 
 after(async () => {
