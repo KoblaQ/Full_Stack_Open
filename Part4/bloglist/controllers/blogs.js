7273d44da17ae529b61d43422e762e7a79525e3e
Edem <edem.quashigah@student.hamk.fi>
2025-11-10 13:52:11 +0200

    4.18
    User identified by token is designated as the creator of the blog.

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,14 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+// Get all blogs
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 }) // .populate works like join in SQL
   response.json(blogs)
 })
 
+// Extract token from authorization header
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+// Create a blog
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id) // Get the user through the decoded token id
+  // const user = await User.findById(body.userId) // Find the user
+  if (!user) {
+    return response.status(400).json({ error: 'usesrId missing or not valid' })
+  }
+
   // Adds like property at value 0 if omitted
   if (!body.likes) {
     body.likes = 0
@@ -19,14 +42,10 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
-  const user = await User.findById(body.userId) // Find the user
-  if (!user) {
-    return response.status(400).json({ error: 'usesrId missing or not valid' })
-  }
-
   const blog = new Blog({
     title: body.title,
-    author: body.author,
+    // author: body.author,
+    author: user.name, // Set the author of the blog to the name of the user creating it
     url: body.url,
     user: user._id,
     likes: body.likes || 0,
