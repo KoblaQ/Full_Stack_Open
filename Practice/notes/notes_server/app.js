d1ebb1fbca3562cfd63a30ae059ec690d902c4e5
Edem <edem.quashigah@student.hamk.fi>
2025-12-02 16:41:24 +0200

    refactoring for tests to reset and use the testing database

--- Practice/notes/notes_server/app.js
+++ Practice/notes/notes_server/app.js
@@ -28,6 +28,12 @@ app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+// Only use the testing router if it is started from the playwright test
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
