5ea48ccac49ff389f34740bb321715b9c9490a82
Edem <edem.quashigah@student.hamk.fi>
2025-12-04 18:08:08 +0200

    5.19 tests for login (both successful and failed)

--- Part5/bloglist-e2e/tests/bloglist_app.spec.js
+++ Part5/bloglist-e2e/tests/bloglist_app.spec.js
@@ -1,7 +1,21 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    //Empty the database (from the reset controler in the backend)
+    await request.post('http://localhost:3003/api/testing/reset')
+
+    // Create a user for the backend
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'KoblaQ',
+        password: 'salainen',
+      },
+    })
+
+    // Go to the login page
     await page.goto('http://localhost:5173')
   })
 
@@ -14,5 +28,28 @@ describe('Blog app', () => {
     await expect(usernameField).toBeVisible()
     await expect(passwordField).toBeVisible()
     await expect(loginButton).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'KoblaQ', 'salainen')
+
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+      await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'wrongUsername', 'wrongPassword')
+
+      // Error message should be printed at the right place
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)') // Colors must be defined in playwright as rgb codes.
+
+      // Should still remain on the login page
+      await expect(page.getByText('Test User logged in')).not.toBeVisible()
+      await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    })
   })
 })
