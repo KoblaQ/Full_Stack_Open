c3c51b2723df6029fba29ac9be88dddc81faeb42
Edem <edem.quashigah@student.hamk.fi>
2025-12-10 14:05:49 +0200

    6.3 completed

--- Practice/redux/src/App.jsx
+++ Practice/redux/src/App.jsx
@@ -1,30 +1,115 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+// import { createStore } from 'redux'
+// // import noteReducer from './reducers/noteReducer'
+// import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+// import { useSelector, useDispatch } from 'react-redux'
+import NoteForm from './components/NoteForm'
+import Notes from './components/Notes'
 
-function App() {
-  const [count, setCount] = useState(0)
+// const counterReducer = (state, action) => {
+//   if (action.type === 'INCREMENT') {
+//     return state + 1
+//   } else if (action.type === 'DECREMENT') {
+//     return state - 1
+//   } else if (action.type === 'ZERO') {
+//     return 0
+//   }
 
+//   return state
+// }
+
+// const counterReducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     case 'ZERO':
+//       return 0
+//     default: // If none of the above matches, code comes here
+//       return state
+//   }
+// }
+
+// const noteReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case 'NEW_NOTE':
+//       // state.push(action.payload)
+//       return state.concat(action.payload)
+//     // case 'TOGGLE_IMPORTANCE':
+//     //   return state.concat()
+//     default:
+//       return state
+//   }
+// }
+
+// const store = createStore(counterReducer)
+// const store = createStore(noteReducer)
+// store.subscribe(() => {
+//   const storeNow = store.getState()
+//   console.log(storeNow)
+// })
+// console.log(store.getState())
+// store.dispatch({ type: 'INCREMENT' })
+// store.dispatch({ type: 'INCREMENT' })
+// store.dispatch({ type: 'INCREMENT' })
+
+// // console.log(store.getState())
+// store.dispatch({ type: 'ZERO' })
+// store.dispatch({ type: 'DECREMENT' })
+// // console.log(store.getState())
+
+// useDispatch(
+//   createNote({
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content: 'the app state is in redux store',
+//       important: true,
+//       id: 1,
+//     },
+//   })
+// )
+
+// useSelector(
+//   createNote({
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content: 'state changes are made with acions',
+//       important: false,
+//       id: 2,
+//     },
+//   })
+// )
+
+const App = () => {
+  // const dispatch = useDispatch()
+  // const notes = useSelector((state) => state)
+
+  // const addNote = (event) => {
+  //   event.preventDefault()
+  //   const content = event.target.note.value
+  //   event.target.note.value = ''
+  //   dispatch(createNote(content))
+  // }
+
+  // const toggleImportance = (id) => {
+  //   dispatch(toggleImportanceOf(id))
+  // }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form> */}
+      <NoteForm />
+      <Notes />
+      {/* <ul>
+        {notes.map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul> */}
+    </div>
   )
 }
 
--- Practice/redux/src/main.jsx
+++ Practice/redux/src/main.jsx
@@ -1,107 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App'
 import noteReducer from './reducers/noteReducer'
 
-// const counterReducer = (state, action) => {
-//   if (action.type === 'INCREMENT') {
-//     return state + 1
-//   } else if (action.type === 'DECREMENT') {
-//     return state - 1
-//   } else if (action.type === 'ZERO') {
-//     return 0
-//   }
-
-//   return state
-// }
-
-// const counterReducer = (state = 0, action) => {
-//   switch (action.type) {
-//     case 'INCREMENT':
-//       return state + 1
-//     case 'DECREMENT':
-//       return state - 1
-//     case 'ZERO':
-//       return 0
-//     default: // If none of the above matches, code comes here
-//       return state
-//   }
-// }
-
-// const noteReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case 'NEW_NOTE':
-//       // state.push(action.payload)
-//       return state.concat(action.payload)
-//     // case 'TOGGLE_IMPORTANCE':
-//     //   return state.concat()
-//     default:
-//       return state
-//   }
-// }
-
-// const store = createStore(counterReducer)
 const store = createStore(noteReducer)
-// store.subscribe(() => {
-//   const storeNow = store.getState()
-//   console.log(storeNow)
-// })
-// console.log(store.getState())
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'INCREMENT' })
-// store.dispatch({ type: 'INCREMENT' })
 
-// // console.log(store.getState())
-// store.dispatch({ type: 'ZERO' })
-// store.dispatch({ type: 'DECREMENT' })
-// // console.log(store.getState())
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with acions',
-    important: false,
-    id: 2,
-  },
-})
-
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-    // <div>
-    //   <div>{store.getState()}</div>
-    //   <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
-    //     plus
-    //   </button>
-    //   <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
-    //     minus
-    //   </button>
-    //   <button onClick={() => store.dispatch({ type: 'ZERO' })}>zero</button>
-    // </div>
-  )
-}
-
-const root = createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
