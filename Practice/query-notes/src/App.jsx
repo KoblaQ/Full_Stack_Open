93476576ca4e310c80aa891d18474013877650d7
Edem <edem.quashigah@student.hamk.fi>
2025-12-14 12:50:44 +0200

    Practice before 6.20

--- Practice/query-notes/src/App.jsx
+++ Practice/query-notes/src/App.jsx
@@ -1,16 +1,52 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests'
+
 const App = () => {
+  const queryClient = useQueryClient()
+
+  // Mutation is React query's way of CRUD without the R
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      // queryClient.invalidateQueries({ queryKey: ['notes'] })
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    },
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    // console.log(content)
+    newNoteMutation.mutate({ content, important: true }) // POST a new note
   }
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] }) // Causes the re-rendering of the notes by rendering the current one invalid
+    },
+  })
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important })
+    // console.log('toggle importance of', note.id)
   }
 
-  const notes = []
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false, // disable refetching when window is focused
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
 
   return (
     <div>
--- Practice/query-notes/src/main.jsx
+++ Practice/query-notes/src/main.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')).render(
+  // wrap the App in the QueryClientProvider to make it accessible to everything
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
