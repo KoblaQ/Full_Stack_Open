1f60ef3c04f1ef42a13e780d1faed810d3a68d1a
Edem <edem.quashigah@student.hamk.fi>
2025-12-27 01:46:52 +0200

    webpack dev server implemented

--- Practice/webpack/src/index.js
+++ Practice/webpack/src/index.js
@@ -3,11 +3,11 @@ import 'regenerator-runtime/runtime.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// import './index.css'
 
 const hello = (name) => {
   console.log(`hello ${name}`)
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-App()
+// App()
