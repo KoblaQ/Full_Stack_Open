c72e878e8e15792ea0f826aff79579171d3f7b12
Edem <edem.quashigah@student.hamk.fi>
2025-12-27 02:37:37 +0200

    Development and production configuration

--- Practice/webpack/src/index.js
+++ Practice/webpack/src/index.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import './index.css'
+import './index.css'
 
 const hello = (name) => {
   console.log(`hello ${name}`)
