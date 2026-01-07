fdfee0451509b280b932ac40c07845533ee5c0c5
Edem <edem.quashigah@student.hamk.fi>
2026-01-07 15:52:19 +0200

    9.3 COMPLETED parameters are taken from cli

--- Part9/calculator/bmiCalculator.ts
+++ Part9/calculator/bmiCalculator.ts
@@ -1,40 +1,68 @@
 type Result = string
 
+interface bmiValues {
+  height: number
+  weight: number
+}
+
+const parseArgumentss = (args: string[]): bmiValues => {
+  console.log('WORKING HERE')
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers')
+  }
+}
+
 const calculateBmi = (height: number, weight: number): Result => {
-  console.log('height', height)
-  console.log('weight', weight)
+  // console.log('height', height)
+  // console.log('weight', weight)
   // return 'Normal range'
   const heightInMeters: number = height / 100
 
   const bmi: number = weight / (heightInMeters * heightInMeters)
-  console.log('bmi', bmi)
+  // console.log('bmi', bmi)
+  let bmiMessage = ''
 
   if (bmi < 16.0) {
     return 'Underweight (Severe thinness)'
   } else if (bmi >= 16.0 && bmi < 17.0) {
-    return 'Underweight (Moderate thinness)'
+    bmiMessage = 'Underweight (Moderate thinness)'
   } else if (bmi >= 17.0 && bmi < 18.5) {
-    return 'Underweight (Mild thinness)'
+    bmiMessage = 'Underweight (Mild thinness)'
   } else if (bmi >= 18.5 && bmi < 25.0) {
-    return 'Normal range'
+    bmiMessage = 'Normal range'
   } else if (bmi >= 25.0 && bmi < 30.0) {
-    return 'Overweight (Pre-obese)'
+    bmiMessage = 'Overweight (Pre-obese)'
   } else if (bmi >= 30.0 && bmi < 35.0) {
-    return 'Obese (Class I)'
+    bmiMessage = 'Obese (Class I)'
   } else if (bmi >= 35.0 && bmi < 40.0) {
-    return 'Obese (Class II)'
+    bmiMessage = 'Obese (Class II)'
   } else if (bmi >= 40.0) {
-    return 'Obese (Class III)'
+    bmiMessage = 'Obese (Class III)'
   } else {
-    return 'Something went wrong'
+    bmiMessage = 'Something went wrong'
   }
+
+  console.log(bmiMessage)
 }
 
 try {
-  console.log(calculateBmi(180, 74))
+  const { height, weight } = parseArgumentss(process.argv)
+  // console.log(calculateBmi(height, weight))
+  calculateBmi(height, weight)
 } catch (error: unknown) {
   let errorMessage = 'Something went wrong: '
   if (error instanceof Error) {
     errorMessage += error.message
   }
+  console.log(errorMessage)
 }
+
+// console.log(process.argv.length)
--- Part9/calculator/exerciseCalculator.ts
+++ Part9/calculator/exerciseCalculator.ts
@@ -8,7 +8,29 @@ interface Results {
   average: number
 }
 
-const calculateExercises = (hours: number[], target: number) => {
+interface ExerciseValues {
+  target: number
+  hours: number[]
+}
+
+const parseArguments = (args: string[]): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  // if (args.length > 4) throw new Error('Too many arguments')
+
+  const hours = args.slice(3).map(Number) // Slice from index 3 to get all exercise hours
+  // console.log(hours)
+
+  if (!isNaN(Number(args[2])) && hours.every((hour) => !isNaN(hour))) {
+    return {
+      target: Number(args[2]),
+      hours: hours.map((hour) => Number(hour)),
+    }
+  } else {
+    throw new Error('Provided values were not all numbers!')
+  }
+}
+
+const calculateExercises = (target: number, hours: number[]) => {
   const periodLength = hours.length
   const trainingDays = hours.filter((hour) => hour > 0).length
   const average = hours.reduce((a, b) => a + b, 0) / hours.length
@@ -41,7 +63,7 @@ const calculateExercises = (hours: number[], target: number) => {
       ratingDescription = 'nothing to rate here'
   }
 
-  return {
+  const result = {
     periodLength,
     trainingDays,
     success,
@@ -50,13 +72,17 @@ const calculateExercises = (hours: number[], target: number) => {
     target,
     average,
   }
+  console.log(result)
 }
 
 try {
-  console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+  // console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+  const { target, hours } = parseArguments(process.argv)
+  calculateExercises(target, hours)
 } catch (error: unknown) {
   let errorMessage = 'Something went wrong: '
   if (error instanceof Error) {
     errorMessage += error.message
   }
+  console.log(errorMessage)
 }
