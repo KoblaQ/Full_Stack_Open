2cf2afe80523287c924794bb1b00525481dab606
Edem <edem.quashigah@student.hamk.fi>
2026-01-10 14:55:48 +0200

    9.10 Fetch all diagnoses

--- Part9/patientor-backend/src/index.ts
+++ Part9/patientor-backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged me');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
