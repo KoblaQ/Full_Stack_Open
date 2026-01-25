4fb37f31124a311e2fd3ca93c42c4e69cb916441
Edem <edem.quashigah@student.hamk.fi>
2026-01-25 23:03:01 +0200

    9.18
    Introduced a form in App.tsx to allow users to add new diary entries. Added createEntry service in diaryServices.ts to handle POST requests for new entries. The new entry is appended to the list upon successful creation.

--- Part9/flight-diary-frontend/src/App.tsx
+++ Part9/flight-diary-frontend/src/App.tsx
@@ -1,24 +1,101 @@
 import { useEffect, useState } from 'react';
-import { getAllEntries } from './services/diaryServices';
-import type { DiaryEntry } from './types';
+import { getAllEntries, createEntry } from './services/diaryServices';
+import type { DiaryEntry, NewDiaryEntry } from './types';
 
 function App() {
   // const [entry, setEntry] = useState('')
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
+  const [newEntry, setNewEntry] = useState({
+    date: '',
+    weather: '',
+    visibility: '',
+    comment: '',
+  });
 
   useEffect(() => {
     getAllEntries().then((data) => {
       setEntries(data);
     });
   }, []);
-  console.log(entries);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    setNewEntry((newEntry) => ({ ...newEntry, [name]: value }));
+  };
+
+  const entryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    createEntry(newEntry).then((data: DiaryEntry) => {
+      setEntries(entries.concat(data));
+    });
+    console.log(newEntry);
+    // const date = event.target.date;
+    setNewEntry({
+      date: '',
+      weather: '',
+      visibility: '',
+      comment: '',
+    });
+  };
 
   return (
     <div>
+      <h3>Add new entry</h3>
+      <form onSubmit={entryCreation}>
+        <div>
+          <label htmlFor="date">
+            date
+            <input
+              type="text"
+              name="date"
+              value={newEntry.date}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="visibility">
+            visibility
+            <input
+              type="text"
+              name="visibility"
+              value={newEntry.visibility}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="weather">
+            weather
+            <input
+              type="text"
+              name="weather"
+              value={newEntry.weather}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="comment">
+            comment
+            <input
+              type="text"
+              name="comment"
+              value={newEntry.comment}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <button type="submit">add</button>
+      </form>
+
       <h3>Diary entries</h3>
       {entries.map((entry) => (
-        <div>
-          <h4 key={entry.id}>{entry.date}</h4>
+        <div key={entry.id}>
+          <h4>{entry.date}</h4>
           <p>visibility: {entry.visibility}</p>
           <p>weather: {entry.weather}</p>
           <p>comments: {entry.comment}</p>
