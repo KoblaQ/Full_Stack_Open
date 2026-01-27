418471ad8f12a8c2b6bf5d157626fe23cda9d4ec
Edem <edem.quashigah@student.hamk.fi>
2026-01-27 18:28:33 +0200

    9.23

--- Part9/patientor-backend/src/types.ts
+++ Part9/patientor-backend/src/types.ts
@@ -1,20 +1,64 @@
 import { z } from 'zod';
 import { NewEntrySchema } from './utils';
 
-export type Diagnosis = {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
-};
+}
 
 export enum Gender {
-  male = 'male',
-  female = 'female',
-  other = 'other',
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+// export interface Entry {}
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  // diagnosisCodes?: string[];
+  // diagnosisCode?: Diagnosis['code'][];
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -34,3 +78,6 @@ export type NewPatientEntry = z.infer<typeof NewEntrySchema>;
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 // export type NewPatientEntry = Omit<Patient, 'id'>; // REPLACED WITH THE ZOD INFER
+export type EntryWithoutId = Omit<Entry, 'id'>;
+
+// export type EntryWithoutId = UnionOmit<Entry, 'id'>;
